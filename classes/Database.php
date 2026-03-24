c097b9b7c5cf672f9380bdebaedd572d7361970e
Leonel90-34 <balbosaleonel2@gmail.com>
2026-03-24 21:02:10 +0800

    Move DB credentials initialization to constructor
    Refactor database connection properties to be set in the constructor.

--- classes/Database.php
+++ classes/Database.php
@@ -1,12 +1,17 @@
 <?php
 class Database {
-    private $host = getenv('DB_HOST');
-    private $user = getenv('DB_USER');
-    private $pass = getenv('DB_PASS');
-    private $dbname = getenv('DB_NAME');
+    private $host;
+    private $user;
+    private $pass;
+    private $dbname;
     public $conn;
 
     public function __construct() {
+        $this->host = getenv('DB_HOST');
+        $this->user = getenv('DB_USER');
+        $this->pass = getenv('DB_PASS');
+        $this->dbname = getenv('DB_NAME');
+        
         try {
             $this->conn = new PDO(
                 "mysql:host={$this->host};dbname={$this->dbname}",
